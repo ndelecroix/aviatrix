7376876e49c828ccc35b403a8233e5284fcd5b79
ndelecroix <66386409+ndelecroix@users.noreply.github.com>
2020-08-13 21:02:43 -0700

    Use aws_route instead of aws_route_table to define all routes

--- custom-vpc-rt/custom-vpc-rt-aws.tf
+++ custom-vpc-rt/custom-vpc-rt-aws.tf
@@ -34,10 +34,6 @@ resource "aws_subnet" "subnets" {
 ### Route tables.
 resource "aws_route_table" "rt1" {
   vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
   tags = {
     Name = var.rt1.name
   }
@@ -59,6 +55,13 @@ resource "aws_route_table_association" "rt1_subnet1" {
 resource "aws_route_table_association" "rt2_subnet2" {
   subnet_id      = aws_subnet.subnets["subnet2"].id
   route_table_id = aws_route_table.rt2.id
+}
+
+### Default route to IGW for RT1.
+resource "aws_route" "rt1_igw_route" {
+  route_table_id         = aws_route_table.rt1.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 ### RFC 1918 routes pointing to Spoke GW ENI in RT1, once the Spoke GW
